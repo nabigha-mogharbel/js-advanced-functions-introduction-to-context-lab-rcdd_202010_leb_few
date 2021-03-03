ae5495a833e5329221214f6117e24f51af39cae0
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-03-03 20:32:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// Your co
+function createEmployeeRecord(array){
+  let worker={
+    firstName:array[0],
+    familytName:array[1],
+    title:array[2],
+    payPerHour:array[3],
+    timeInEvents:[],
+    timeOutevents:[]
+  }
+  return worker
+}
+
+function createEmployeeRecords(anoArray){
+  let array=anoArray.map(ele => createEmployeeRecord(ele))
+}
+
+function createTimeInEvent(workerRecord, dataStamp){
+  let record={}
+  record.type="TimeIn";
+  record.hour=parseInt(dataStamp.substring(11));
+  record.date=parseInt(dataStamp.substring(0,10));
+  workerRecord.timeInEvents.push(record)
+  return workerRecord
+}
+
+function createTimeOutEvent(workerRecord, dataStamp){
+  let record={}
+  record.type="TimeOut";
+  record.hour=parseInt(dataStamp.substring(11));
+  record.date=parseInt(dataStamp.substring(0,10));
+  workerRecord.timeOutEvents.push(record)
+  return workerRecord
+}
